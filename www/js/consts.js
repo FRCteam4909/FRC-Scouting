4bf10d27b21f068032daa11563ead3481a83e053
Roshan Ravi <roshanravi10@gmail.com>
2016-12-16 18:31:17 -0500

    JS Constants

--- www/js/consts.js
+++ www/js/consts.js
@@ -1,4 +1,8 @@
 const config = {
 	sendFolder: "/frc-scout/transfer/toHub/",
-	receiveFolder: "/frc-scout/transfer/toDevice/"
+	receiveFolder: "/frc-scout/transfer/toDevice/",
+	
+	//* Enables Debugging Logs
+	testing: true
+	//*/
 };
