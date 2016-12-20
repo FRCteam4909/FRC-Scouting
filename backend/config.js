e2afb10cb395f79f26c29e3adde86dee2eada93a
Roshan Ravi <roshanravi10@gmail.com>
2016-12-19 22:15:52 -0500

    Reconfigure Data Directories

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	device_directory: "~/FRC-Scouting/dev",
+	receive_directory: "~/FRC-Scouting/dev/frc-scout/transfer/toHub/",
+	send_directory: "~/FRC-Scouting/dev/frc-scout/transfer/toDevice/"
+};
