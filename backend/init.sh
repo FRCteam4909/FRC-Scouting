d989bf78f66e732eb717e06122c5dc45762e485d
Roshan Ravi <roshanravi10@gmail.com>
2016-12-26 23:14:12 -0500

    Add MongoDB Configuration into Initialization Script

--- backend/init.sh
+++ backend/init.sh
@@ -6,7 +6,7 @@
 #
 # chmod +x ./init.sh
 
-echo "THIS COMMAND MUST BE RUN FROM WITHIN THE ./backend directory"
+echo "THIS COMMAND MUST BE AS SUDO RUN FROM WITHIN THE ./backend directory"
 
 echo "Installing Node.JS & NPM"
 sudo apt-get install nodejs -y
@@ -14,6 +14,13 @@ sudo apt-get install npm -y
 
 echo "Installing OBEXFS Bluetooth Mount Utility"
 sudo apt-get install obexfs -y
+
+echo "Installing and Configuring MongoDB"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
 
 echo "=========="
 
