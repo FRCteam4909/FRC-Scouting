ffe7bb7c4e0d7c12d75fdb6d06310e76149a6c43
Roshan Ravi <roshanravi10@gmail.com>
2016-12-26 20:40:17 -0500

    Encapsulate Device Logic within Main Try-Catch Handler

--- backend/main.js
+++ backend/main.js
@@ -1,62 +1,62 @@
 const OBEX_utils = require('./obex_utils'),
-	  sleep = require('./sleep'),
-	  
-	  expandHomeDir = require('expand-home-dir'),
-	  fs = require('fs'),
-	  
-	  config = require('./config');
+	sleep = require('./sleep'),
+
+	expandHomeDir = require('expand-home-dir'),
+	fs = require('fs'),
+
+	config = require('./config');
 
 function pollForNewData(devices) {
-	if(!devices)
+	if (!devices)
 		return;
-	
-	for(macAddr in devices){
+
+	for (macAddr in devices) {
 		// Mount Device and Attempt to Read Directory for New Data
 		try {
 			const device = OBEX_utils.mount(macAddr, expandHomeDir(config.device_directory)),
 
-			files = fs.readdirSync(
-				expandHomeDir(config.receive_directory)
-			);
-		} catch (e) {
-			// TODO: DUMP ERRORS TO LOG
-		}
+				files = fs.readdirSync(
+					expandHomeDir(config.receive_directory)
+				);
 
-		sleep(1);
+			sleep(1);
 
-		files.forEach(function (file) {
+			files.forEach(function (file) {
+				try {
+					const filePath = expandHomeDir(config.receive_directory) + file,
+
+						newFile = fs.readFileSync(filePath, {
+							encoding: "utf8"
+						}),
+
+						data = JSON.parse(newFile);
+
+					if (data.check != "9dcec4e5sd7f890s")
+						throw new Error("Error Parsing Data...");
+
+					// Log File to MongoDB
+
+					fs.unlinkSync(filePath);
+				} catch (e) {
+					// TODO: DUMP ERRORS TO LOG
+				}
+			});
+
+			sleep(1);
+
 			try {
-				const filePath = expandHomeDir(config.receive_directory) + file,
-
-					  newFile = fs.readFileSync(filePath, {
-						  encoding: "utf8"
-					  }),
-
-					  data = JSON.parse(newFile);
-
-				if (data.check != "9dcec4e5sd7f890s")
-					throw new Error("Error Parsing Data...");
-
-				// Log File to MongoDB
-
-				fs.unlinkSync(filePath);
+				OBEX_utils.unmount(device);
 			} catch (e) {
 				// TODO: DUMP ERRORS TO LOG
 			}
-		});
 
-		sleep(1);
-
-		try {
-			OBEX_utils.unmount(device);
+			sleep(4);
 		} catch (e) {
 			// TODO: DUMP ERRORS TO LOG
 		}
-		
-		sleep(4);
 	}
-	
-	setTimeout(function(){
+
+	setTimeout(function () {
 		pollForNewData(devices);
 	}, 5000);
 }
