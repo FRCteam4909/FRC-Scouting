557c566a58bc60928740be225c1f5604d546ae7d
Roshan Ravi <roshanravi10@gmail.com>
2017-03-31 21:18:35 -0400

    Work on Data Input

--- backend/www/js/index.js
+++ backend/www/js/index.js
@@ -74,40 +74,41 @@ function loadData(index) {
                     cols = view.cols,
                     min = [], max = [];
 
-                cols.forEach((col, colIndex)=>{
-                    table.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
-                        var cell = table.cell({ row: rowIdx, column: col}).node(),
-                            cellVal = Number(cell.innerHTML);
+                if(rootView.colorcode)
+                    cols.forEach((col, colIndex)=>{
+                        table.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
+                            var cell = table.cell({ row: rowIdx, column: col}).node(),
+                                cellVal = Number(cell.innerHTML);
 
-                        if(!Number.isNaN(cellVal)) {
-                            if(cellVal > max[colIndex] || (typeof max[colIndex] === 'undefined'))
-                                max[colIndex] = cellVal;
-                            if(cellVal <= min[colIndex] || (typeof min[colIndex] === 'undefined'))
-                                min[colIndex] = cellVal;
-                        }
-                    }).every( function ( rowIdx, tableLoop, rowLoop ) {
-                        var cell = table.cell({ row: rowIdx, column: col}).node(),
-                            cellVal = Number(cell.innerHTML);
-                            if(min[colIndex] !== max[colIndex])
-                                scaledVal = scaleBetween(cellVal, 0, 1, min[colIndex], max[colIndex]);
+                            if(!Number.isNaN(cellVal)) {
+                                if(cellVal > max[colIndex] || (typeof max[colIndex] === 'undefined'))
+                                    max[colIndex] = cellVal;
+                                if(cellVal <= min[colIndex] || (typeof min[colIndex] === 'undefined'))
+                                    min[colIndex] = cellVal;
+                            }
+                        }).every( function ( rowIdx, tableLoop, rowLoop ) {
+                            var cell = table.cell({ row: rowIdx, column: col}).node(),
+                                cellVal = Number(cell.innerHTML);
+                                if(min[colIndex] !== max[colIndex])
+                                    scaledVal = scaleBetween(cellVal, 0, 1, min[colIndex], max[colIndex]);
+                                else
+                                    scaledVal = 0.6;
+                            /*
+                            console.log(rowIdx);
+                            console.log(col);
+                            console.log(cellVal);
+                            console.log(min[colIndex]);
+                            console.log(max[colIndex]);
+                            console.log(scaledVal);
+                            console.log("=====");
+                            */
+
+                            if(scaledVal > 0.5)
+                                $(cell).css('background-color', 'rgba(76, 217, 100,' + (0.1 + (0.8 * scaledVal)) + ')');
                             else
-                                scaledVal = 0.6;
-                        /*
-                        console.log(rowIdx);
-                        console.log(col);
-                        console.log(cellVal);
-                        console.log(min[colIndex]);
-                        console.log(max[colIndex]);
-                        console.log(scaledVal);
-                        console.log("=====");
-                        */
-
-                        if(scaledVal > 0.5)
-                            $(cell).css('background-color', 'rgba(76, 217, 100,' + (0.1 + (0.8 * scaledVal)) + ')');
-                        else
-                            $(cell).css('background-color', 'rgba(255, 59, 48,' + (0.4 - (scaledVal)) + ')');
+                                $(cell).css('background-color', 'rgba(255, 59, 48,' + (0.4 - (scaledVal)) + ')');
+                        });
                     });
-                })
             });
             
             searchColumn(0, $("#findTeamNumber").val());
--- config/views.js
+++ config/views.js
@@ -5,24 +5,8 @@ module.exports = {
             {
                 "name": "teamAverages",
                 "disableInfo": true,
-                "disablePaging": true
-            },
-            {
-                "name": "comments",
-                "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ]
-            }
-        ]
-	},{
-		"name": "Overall Team Profile",
-		"views": [
-            {
-                "name": "teamAverages",
-                "disableInfo": true,
-                "disablePaging": true
-            },
-            {
-                "name": "matches",
-                "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ]
+                "disablePaging": true,
+                "colorcode": true
             },
             {
                 "name": "comments",
@@ -35,16 +19,8 @@ module.exports = {
             {
                 "name": "teamAverages",
                 "disableInfo": true,
-                "disablePaging": true
-            }
-        ]
-	},{
-		"name": "Team Consistencies",
-		"views": [
-            {
-                "name": "teamConsistency",
-                "disableInfo": true,
-                "disablePaging": true
+                "disablePaging": true,
+                "colorcode": true
             }
         ]
 	},{
@@ -56,7 +32,34 @@ module.exports = {
                 "disablePaging": true
             }
         ]
-	},],
+	},{
+		"name": "Overall Team Profile",
+		"views": [
+            {
+                "name": "teamAverages",
+                "disableInfo": true,
+                "disablePaging": true,
+                "colorcode": true
+            },
+            {
+                "name": "matches",
+                "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ]
+            },
+            {
+                "name": "comments",
+                "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ]
+            }
+        ]
+	},{
+		"name": "Raw Match Data",
+		"views": [
+            {
+                "name": "matches",
+                "disableInfo": true,
+                "disablePaging": true
+            }
+        ]
+	}],
 	
     tableViews: {
         
@@ -70,19 +73,15 @@ module.exports = {
 				callback([
 					{
                         name: "Team Matches",
-                        cols: [2,3,4,5,6,7,8,9,10,11,12,14],
+                        cols: [2,3,4,5,6,7,8,9,10,11],
                         headers: [
                             {
                                 text: "Team", 
-                                value: "team"	
+                                value: "team"
                             },
                             {
                                 text: "Event", 
                                 value: "event"	
-                            },
-                            {
-                                text: "Crossed Baseline", 
-                                value: "baseline"	
                             },
                             {
                                 text: "Auto. Low Goal kPa",
@@ -127,10 +126,6 @@ module.exports = {
                             {
                                 text: "Comments", 
                                 value: "comments"	
-                            },
-                            {
-                                text: "Do Not Pick", 
-                                value: "do-not-pick"	
                             }
                         ],
                         data: matches
@@ -271,102 +266,6 @@ module.exports = {
                         {
                             text: "Climber Success Rate",
                             value: "touchpad-success"
-                        }
-                    ],
-                    data: matches
-                }
-            ]);
-            
-            db.close();
-        });
-	},
-    
-    teamConsistency: function (mongoCollection, callback) {
-        mongoCollection.aggregate([
-          {
-              $match: {"competition": 2017}  
-          },
-          {
-              $group: {
-                  _id: {
-                      "team": '$team',
-                      "event": '$event'
-                  },
-                  
-                  "baseline": { $stdDevPop: '$baseline' },
-                  
-                  "auto-low-kPa": { $stdDevPop: '$auto-low-kPa' },
-                  "auto-high-kPa": { $stdDevPop: '$auto-high-kPa' },
-                  "auto-gears": { $stdDevPop: '$auto-gears' },
-                  
-                  "teleop-low-kPa": { $stdDevPop: '$teleop-low-kPa' },
-                  "teleop-high-kPa": { $stdDevPop: '$teleop-high-kPa' },
-                  "teleop-gears": { $stdDevPop: '$teleop-gears' },
-                  
-                  "touchpad": { $stdDevPop: '$touchpad' },
-                  "touchpad-attempted": { $stdDevPop: '$touchpad-attempted' },
-                  
-                  "auto-drop-gears": { $stdDevPop: '$auto-drop-gears' },
-                  "teleop-drop-gears": { $stdDevPop: '$teleop-drop-gears' }
-              }
-          }
-        ]).toArray(function(err, matches) {
-            callback([
-                {
-                    name: "Team Consistency",
-                    cols: [2,3,4,5,6,7,8,9,10,11,12],
-                    headers: [
-                        {
-                            text: "Team", 
-                            value: "_id.team"	
-                        },
-                        {
-                            text: "Event", 
-                            value: "_id.event"	
-                        },
-                        {
-                            text: "Crossed Baseline", 
-                            value: "baseline"	
-                        },
-                        {
-                            text: "Auto. Low Goal kPa",
-                            value: "auto-low-kPa"
-                        },
-                        {
-                            text: "Auto. High Goal kPa",
-                            value: "auto-high-kPa"
-                        },
-                        {
-                            text: "Auto. Gears Placed",
-                            value: "auto-gears"
-                        },
-                        {
-                            text: "Auto. Gears Dropped",
-                            value: "auto-drop-gears"
-                        },
-                        {
-                            text: "Teleop. Low Goal kPa",
-                            value: "teleop-low-kPa"
-                        },
-                        {
-                            text: "Teleop. High Goal kPa",
-                            value: "teleop-high-kPa"
-                        },
-                        {
-                            text: "Teleop. Gears Placed",
-                            value: "teleop-gears"
-                        },
-                        {
-                            text: "Teleop. Gears Dropped",
-                            value: "teleop-drop-gears"
-                        },
-                        {
-                            text: "Attempted Touchpad",
-                            value: "touchpad-attempted"
-                        },
-                        {
-                            text: "Pressed Touchpad",
-                            value: "touchpad"
                         }
                     ],
                     data: matches
